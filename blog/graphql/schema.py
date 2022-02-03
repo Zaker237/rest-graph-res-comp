46fc8d212f68bb7e78a71209554286571334abe9
Alex Mboutchouang <mboutchouangalex@gmail.com>
2022-02-03 13:29:23 +0100

    init the architecture

---
+++ blog/graphql/schema.py
@@ -0,0 +1,10 @@
+import graphene
+
+import blog.graphql.schemas.blog_schemas as blog_schemas
+
+
+class Query(blog_schemas.Query, graphene.ObjectType):
+    pass
+
+class Mutation(blog_schemas.Mutation, graphene.ObjectType):
+    pass
